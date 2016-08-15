4115bc15770c906120c47bf7e2214c564a38abba
jiaming <jiaming@17shihui.com>
2016-08-15 17:13:04 +0800

    test

--- open-living-service/src/main/java/com/shihui/openpf/living/io3rd/GuangdaResponse.java
+++ open-living-service/src/main/java/com/shihui/openpf/living/io3rd/GuangdaResponse.java
@@ -172,6 +172,8 @@ public class GuangdaResponse {
 		
 		result.put("response", new SimpleResponse(1,"查询成功") );
 		//
+		ApiLogger.info("@@@@GuangdaResponse : noticeApp() : userId :[" + order.getUserId() + "] result : " + result.toJSONString());
+		//
 		appNotice.pushQueryResult(order.getUserId(), result);
 	}
 	private void load_vo_elements(QueryOrderBillVo vo) {
