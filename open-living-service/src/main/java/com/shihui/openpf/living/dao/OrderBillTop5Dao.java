9bb76d0f108518c7a768f53b148f32fde921fbc7
jiaming <jiaming@17shihui.com>
2016-08-29 19:43:58 +0800

    test

--- open-living-service/src/main/java/com/shihui/openpf/living/dao/OrderBillTop5Dao.java
+++ open-living-service/src/main/java/com/shihui/openpf/living/dao/OrderBillTop5Dao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import com.shihui.commons.ApiLogger;
 import com.shihui.openpf.living.entity.OrderBillTop5;
 import com.shihui.openpf.living.entity.support.BillStatusEnum;
 
@@ -14,11 +15,13 @@ import com.shihui.openpf.living.entity.support.BillStatusEnum;
 public class OrderBillTop5Dao extends AbstractDao<OrderBillTop5> {
 	public List<OrderBillTop5> query(long userId, int count) {
 		StringBuilder sql = new StringBuilder();
-		sql.append("SELECT a.order_id, a.user_id, a.group_id, a.mid, a.service_id, a.goods_id, a.goods_version, b.category_id, b.fee_name, b.city_id, b.company_id, b.company_no, b.bill_key, b.bill_key_type ");
+		sql.append("SELECT a.order_id, a.user_id, a.gid AS group_id, a.mid, a.service_id, a.goods_id, a.goods_version, b.category_id, b.fee_name, b.city_id, b.company_id, c.company_no, b.bill_key, b.bill_key_type ");
 		sql.append("FROM `order` AS a LEFT JOIN `bill` AS b ON a.order_id = b.order_id ");
+		sql.append("LEFT JOIN `company` AS c ON c.company_id = b.company_id  ");
 //		sql.append("WHERE b.bill_status = ? AND a.user_id = ? ORDER BY b.pay_time DESC  LIMIT ? ");
 		sql.append("WHERE a.user_id = ? ORDER BY b.pay_time DESC  LIMIT ? ");
 
+		ApiLogger.info("top5 : sql : " + sql.toString());
 //		return this.queryForList(sql.toString(), new Object[]{BillStatusEnum.CheckSuccess.getValue(),userId, count});
 		return this.queryForList(sql.toString(), new Object[]{userId, count});
 	}
