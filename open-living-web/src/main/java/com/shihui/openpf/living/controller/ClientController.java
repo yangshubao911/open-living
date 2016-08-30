a1aa054ad141069887633791e4ce840c5ac69e2e
jiaming <jiaming@17shihui.com>
2016-08-30 19:21:28 +0800

    test

--- open-living-web/src/main/java/com/shihui/openpf/living/controller/ClientController.java
+++ open-living-web/src/main/java/com/shihui/openpf/living/controller/ClientController.java
@@ -117,8 +117,8 @@ public class ClientController extends BasicController {
 			@RequestParam(name="userNo", required = true) String userNo,
 			@RequestParam(name="field2", required = true) String field2) {
 //TODO XXX
-if(true)
-	return null;
+//if(true)
+//	return null;
 		ApiLogger.info("Controller: /v2/openpf/living/app/queryFee : queryFee() : "
 				+ "userId: " + userId
 				+ "groupId: " + groupId
@@ -170,8 +170,8 @@ if(true)
 			@RequestParam(name="tempId", required = true) String tempId,
 			@RequestParam(name="costSh", required = true) Integer costSh) {
 //TODO XXX
-if(true)
-	return null;
+//if(true)
+//	return null;
 		ApiLogger.info("Controller: /v2/openpf/living/app/createOrder : createOrder() : "
 				+ "userId: " + userId
 				+ "tempId: " + tempId
