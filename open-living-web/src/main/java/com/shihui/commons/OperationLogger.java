87ca8001836470e4296a02015d8dc2b2ed7336e0
jiaming <jiaming@17shihui.com>
2016-08-31 15:32:16 +0800

    test

--- open-living-web/src/main/java/com/shihui/commons/OperationLogger.java
+++ open-living-web/src/main/java/com/shihui/commons/OperationLogger.java
@@ -5,8 +5,7 @@ package com.shihui.commons;
 import com.shihui.api.core.context.RequestContext;
 //import me.weimi.api.commons.logger.CentralLogger;
 import com.shihui.common.logger.CentralLogger;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.shihui.commons.ApiLogger;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -21,7 +20,7 @@ import java.util.Map;
  */
 public class OperationLogger {
     private static CentralLogger centralLogger = CentralLogger.getLogger();
-    private static Logger log = LoggerFactory.getLogger(OperationLogger.class);
+//    private static Logger log = LoggerFactory.getLogger(OperationLogger.class);
 
     public static void log(String action, RequestContext rc, Map<String, Object> expand) {
 //        Map<String, Object> data = new HashMap<>();
@@ -38,31 +37,52 @@ public class OperationLogger {
 //        ApiLogger.info("***CENTRAL LOG: action:[" + action + "] data :[" + data.toString()+"]");
 //        centralLogger.log(action, data);
 
-        if (rc == null) {
-            log.warn("RequestContext not got");
-            return;
-        }
+//        if (rc == null) {
+//            log.warn("RequestContext not got");
+//            return;
+//        }
         OperationLog olog = new OperationLog();
+        ApiLogger.info(" -  1 -");
         olog.setTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        ApiLogger.info(" -  2 -");
         olog.setChannel("kuyue");
+        ApiLogger.info(" -  3 -");
         olog.setClientVersion(rc.getClient().getClientVersion().toString());
+        ApiLogger.info(" -  4 -");
         olog.setDeviceId((String)expand.get("ndeviceid"));
+        ApiLogger.info(" -  5 -");
         olog.setCityId((String)expand.get("cityId"));
+        ApiLogger.info(" -  6 -");
         olog.setGid((String)expand.get("gid"));
+        ApiLogger.info(" -  7 -");
         olog.setServiceId((String)expand.get("serviceId"));
+        ApiLogger.info(" -  8 -");
         olog.setUid(String.valueOf(rc.getUid()));
+        ApiLogger.info(" -  9 -");
         olog.setIp(rc.getIp());
+        ApiLogger.info(" -  10 -");
         if(expand.get("businessId")!=null) {
             Map<String, String> map = new HashMap<>();
+            ApiLogger.info(" -  11 -");
             map.put("businessId",(String)expand.get("businessId"));
             olog.setExpand(map);
+            ApiLogger.info(" -  12 -");
             
             olog.setAction(action + "?businessId=" + expand.get("businessId"));
+            ApiLogger.info(" -  13 -");
+
         } else {
+            ApiLogger.info(" -  14 -");
+
             olog.setAction(action);
+            ApiLogger.info(" -  15 -");
+
         }
-        log.info("TFS LOG:" + JSON.toJSONString(olog));
-        centralLogger.log(action, (JSONObject)JSON.toJSON(olog));    
+//        log.info("TFS LOG:" + JSON.toJSONString(olog));
+      ApiLogger.info("***CENTRAL LOG: action:[" + action + "] data :[" + JSON.toJSONString(olog)+"]");
+        centralLogger.log(action, (JSONObject)JSON.toJSON(olog));   
+        ApiLogger.info(" -  16 -");
+
     }
 
 }
