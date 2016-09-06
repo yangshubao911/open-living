d08c820303925601d7bf5ea9edea0b90fce3df56
jiaming <jiaming@17shihui.com>
2016-09-06 12:24:48 +0800

    test

--- open-living-service/src/main/java/com/shihui/openpf/living/util/ShangHaiChenNanShuiWuUtil.java
+++ open-living-service/src/main/java/com/shihui/openpf/living/util/ShangHaiChenNanShuiWuUtil.java
@@ -6,34 +6,14 @@ public class ShangHaiChenNanShuiWuUtil {
 
 	public static boolean checkUserNo(String userNo) {
 		if(userNo.length() == 24) {
-			String str = String.valueOf(
-					10 - ((
-			(Integer.valueOf(userNo.charAt(0)) 
-			+ Integer.valueOf(userNo.charAt(2)) 
-			+ Integer.valueOf(userNo.charAt(4)) 
-			+ Integer.valueOf(userNo.charAt(6)) 
-			+ Integer.valueOf(userNo.charAt(8)) 
-			+ Integer.valueOf(userNo.charAt(10)) 
-			+ Integer.valueOf(userNo.charAt(12)) 
-			+ Integer.valueOf(userNo.charAt(14)) 
-			+ Integer.valueOf(userNo.charAt(16)) 
-			+ Integer.valueOf(userNo.charAt(18)) 
-			+ Integer.valueOf(userNo.charAt(20)) 
-			+ Integer.valueOf(userNo.charAt(22))) * 3
-			+ Integer.valueOf(userNo.charAt(1))
-			+ Integer.valueOf(userNo.charAt(3))
-			+ Integer.valueOf(userNo.charAt(5))
-			+ Integer.valueOf(userNo.charAt(7)) 
-			+ Integer.valueOf(userNo.charAt(9)) 
-			+ Integer.valueOf(userNo.charAt(11)) 
-			+ Integer.valueOf(userNo.charAt(13)) 
-			+ Integer.valueOf(userNo.charAt(15)) 
-			+ Integer.valueOf(userNo.charAt(17)) 
-			+ Integer.valueOf(userNo.charAt(19)) 
-			+ Integer.valueOf(userNo.charAt(21))
-			))%10
-			);
-			ApiLogger.info("checkUserNo: " + str);
+			int m,i;
+			for(m=0, i=0; i < 23; i += 2)
+				m += Integer.parseInt(userNo.substring(i, i+1));
+			m *= 3;
+			for(i=1; i< 23; i+=2)
+				m += Integer.parseInt(userNo.substring(i, i+1));
+			m = 10 - (m % 10);
+			String str = String.valueOf(m);
 			if( str.charAt(str.length() - 1) == userNo.charAt(userNo.length() - 1))
 					return true;
 		}
